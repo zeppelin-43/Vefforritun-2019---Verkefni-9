5a743d2ec803bfd37b0b27417726d45e8e304213
zeppelin-43 <dantgud@gmail.com>
2019-11-06 19:45:00 +0100

    !

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ const program = (() => {
   let companies;
   let input;
   let results;
-  
+
 
   function newElement(type, text) {
     const el = document.createElement(type);
@@ -19,126 +19,107 @@ const program = (() => {
     return el;
   }
 
-  function cleanUp(){
+  function cleanUp() {
     while (results.firstChild) {
       results.removeChild(results.firstChild);
     }
   }
 
-  function displayCompany(companiesList) {
-    console.log("Display company");
-    if (companiesList.length === 0) {
-      displayError('Fann ekki fyrirtæki');
-      return;
-    }
-
-    
-    cleanUp();
-   
-    for (let i = 0; i < companiesList.length; i++) {
-    const company = companiesList[i];
-    const div = newElement('div');
-
-    const name = company.name;
-    const sn = company.sn;
-    const active = company.active;
-    const address = company.address;
-
-    const dl = newElement('dl', null);
-    const dtName = newElement('dt', 'Lén');
-    const ddName = newElement('dd', name);
-    dl.appendChild(dtName);
-    dl.appendChild(ddName);
-
-    const dtSN = newElement('dt', 'Kennitala');
-    const ddSN = newElement('dd', sn);
-    dl.appendChild(dtSN);
-    dl.appendChild(ddSN);
-
-    if (active === 1) {
-      const dtaddress = newElement('dt', 'Heimilisfang');
-      const ddaddress = newElement('dd', address);
-      dl.appendChild(dtaddress);
-      dl.appendChild(ddaddress);
-      div.className = 'company company--active';
-    }
-    else
-    {
-     div.className = 'company company--inactive';
-    }
-    div.appendChild(dl);
-    results.appendChild(div);
-    
-    
-    }
-  }
-
-
-
   function displayError(error) {
     const container = companies.querySelector('.results');
 
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
+    cleanUp();
 
     container.appendChild(document.createTextNode(error));
+  }
+
+  function displayCompany(companiesList) {
+    if (companiesList.length === 0) {
+      displayError('Ekkert fyrirtæki fannst fyrir leitarstreng');
+      return;
+    }
+
+
+    cleanUp();
+
+    let name;
+    let sn;
+    let active;
+    let address;
+
+    for (let i = 0; i < companiesList.length; i += 1) {
+      const company = companiesList[i];
+      const div = newElement('div');
+
+      name = company.name;
+      sn = company.sn;
+      active = company.active;
+      address = company.address;
+
+      const dl = newElement('dl', null);
+      const dtName = newElement('dt', 'Nafn');
+      const ddName = newElement('dd', name);
+      dl.appendChild(dtName);
+      dl.appendChild(ddName);
+
+      const dtSN = newElement('dt', 'Kennitala');
+      const ddSN = newElement('dd', sn);
+      dl.appendChild(dtSN);
+      dl.appendChild(ddSN);
+
+      if (active === 1) {
+        const dtaddress = newElement('dt', 'Heimilisfang');
+        const ddaddress = newElement('dd', address);
+        dl.appendChild(dtaddress);
+        dl.appendChild(ddaddress);
+        div.className = 'company company--active';
+      } else {
+        div.className = 'company company--inactive';
+      }
+      div.appendChild(dl);
+      results.appendChild(div);
+    }
   }
 
 
   function loading() {
     cleanUp();
-    var div = newElement('div');
+    const div = newElement('div');
     div.className = 'loading';
 
-    var img = newElement('img');
+    const img = newElement('img');
     img.setAttribute('src', 'loading.gif');
 
     div.appendChild(img);
     results.appendChild(div);
   }
 
-   function fetchData(number) {
-    
-  
-
-    const url = API_URL + number;
-    console.log(url);
-    console.log("Fetch data");
-
+  function fetchData(number) {
     loading();
     fetch(`${API_URL}${number}`)
       .then((response) => {
-        console.log(response);
         if (response.ok) {
-          console.log("response.ok");
           return response.json();
         }
-        console.log("Throw error");
-        throw new Error('Villa kom upp');
+        throw new Error('Villa við að sækja gögn');
       })
       .then((data) => {
-        console.log("then data");
         displayCompany(data.results);
       })
       .catch((error) => {
         displayError('Villa!');
-        console.log('error');
-        console.error(error);
-      })
-
+        console.error(error); /* eslint-disable-line */
+      });
   }
 
   function onSubmit(e) {
     e.preventDefault();
-
-    var trimmed = input.value.trim(); // Fjarlægir öll bil vinstra og hægra megin við textann
-    if(trimmed !== '')
+    const trimmed = input.value.trim(); // Fjarlægir öll bil vinstra og hægra megin við textann
+    if (trimmed !== '') {
       fetchData(input.value);
-    else 
+    } else {
       displayError('Fyrirtæki verður að vera strengur');
-    
-
+    }
   }
 
   function init(_companies) {
